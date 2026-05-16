574a7d472bf71ac6015ea2356368d68677ca8fdb
Seifelden Atia <siefeldenabualsoad@gmail.com>
2026-05-16 17:16:00 +0200

    Initial commit

---
+++ src/test/java/com/example/shipmentApp/ShipmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shipmentApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
